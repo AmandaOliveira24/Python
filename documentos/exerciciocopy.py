f2f56879b6958b4f6c6b5e65690c0c1a08682cda
AmandaOliveira24 <amanda.oliveira14@estudante.ifto.edu.br>
2025-06-03 20:33:34 -0300

    Exercício de cópia

---
+++ documentos/exerciciocopy.py
@@ -0,0 +1,31 @@
+import os
+
+
+lista_documentos = os.listdir('PASTA/documentos')
+print(lista_documentos)
+
+# print(lista_arquivos[5])
+
+# # os.rename("arquivos/abr22.txt", "arquivos/22/abr22.txt")
+
+for documentos in lista_documentos:
+    if 'documento' in documentos:
+         if '.docx' in documentos:
+            os.rename(f"C:\\Users\\DBA_Nanda\\Desktop\\python\\PASTA\\documentos\\{documentos}", f"C:\\Users\\DBA_Nanda\\Desktop\\python\\PASTA\\documentos\\word\\{documentos}")
+         elif '.xlsx' in documentos:
+            os.rename(f"C:\\Users\\DBA_Nanda\\Desktop\\python\\PASTA\\documentos\\{documentos}", f"C:\\Users\\DBA_Nanda\\Desktop\\python\\PASTA\\documentos\\excel\\{documentos}")
+         else:
+             os.rename(f"C:\\Users\\DBA_Nanda\\Desktop\\python\\PASTA\\documentos\\{documentos}", f"C:\\Users\\DBA_Nanda\\Desktop\\python\\PASTA\\documentos\\powerpoint\\{documentos}")
+
+
+
+
+
+
+
+
+
+
+
+
+
